5fe75de403d6cc80c3457854b012b1870d082d6f
russelllincallo <85174609+russelllincallo@users.noreply.github.com>
2022-07-08 22:52:13 +0800

    7/8/2022

---
+++ PORTDEMO1/script/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $(window).scroll(function() {
+        if (this.scrollY > 20) {
+            $('.nav-bar').addClass("sticky");
+        } else {
+            $('.nav-bar').removeClass("sticky");
+
+        }
+    })
+});
